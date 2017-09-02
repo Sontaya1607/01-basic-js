52b3ac10deb6e4c73a381915f93cddec6aadfcf9
Sontaya58160434 <58160434@go.buu.ac.th>
2017-09-02 19:54:28 +0700

    Edit Hello.js At 7:54 PM

--- hello.js
+++ hello.js
@@ -2,9 +2,8 @@ function hello(name) {
   return `Hello ${name}`
 }
 
-console.log(hello("golf"));
-// module.exports = hello
-//
-// function sayHi() {
-//
-// }
+module.exports = hello
+
+function sayHi() {
+
+}
